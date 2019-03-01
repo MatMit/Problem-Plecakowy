1f143a22944480622ac5bf1c504420309204201c
MatMit <mati.b6@wp.pl>
2019-03-01 22:31:16 +0100

    Update init.py

--- init.py
+++ init.py
@@ -7,14 +7,26 @@ This is a temporary script file.
 
 import pymysql as mysql
 
-con = mysql.connect(host='localhost',
-                    user='root',
-                    passwd='',
-                    db='db',
-                    charset='utf8mb4')
-with con:
-    cur = con.cursor()
-    cur.execute('SELECT COUNT(id) FROM produkty')
+def init():
+    con = mysql.connect(host='localhost',
+                        user='root',
+                        passwd='',
+                        db='db',
+                        charset='utf8mb4')
+    try:
+        with con:
+            cur = con.cursor()
+            cur.execute('SELECT COUNT(id) FROM produkty')
+            
+            rows = cur.fetchone()[0]
+            if rows == 0:
+                print("Brak wpisow")
+            else:
+                print("Liczba wpisow: "+str(rows))
+        con.close()
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    init()
     
-    print(cur.fetchone()[0])
-con.close()
